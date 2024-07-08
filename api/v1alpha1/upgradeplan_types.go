02662b2b973a26c69190ad0ee24039334a39dfd3
Atanas Dinov <atanas.dinov@suse.com>
2024-07-08 12:12:09 +0300

    Reconcile upgrade plans (#2)
    * Reconcile upgrade plans
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    * Require releaseVersion
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    ---------
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- api/v1alpha1/upgradeplan_types.go
+++ api/v1alpha1/upgradeplan_types.go
@@ -20,16 +20,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // UpgradePlanSpec defines the desired state of UpgradePlan
 type UpgradePlanSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of UpgradePlan. Edit upgradeplan_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// ReleaseVersion specifies the target version for platform upgrade.
+	// The version format is X.Y.Z, for example "3.0.2".
+	ReleaseVersion string `json:"releaseVersion"`
 }
 
 // UpgradePlanStatus defines the observed state of UpgradePlan
--- internal/controller/upgradeplan_controller.go
+++ internal/controller/upgradeplan_controller.go
@@ -39,17 +39,19 @@ type UpgradePlanReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the UpgradePlan object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
 func (r *UpgradePlanReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var upgradePlan lifecyclev1alpha1.UpgradePlan
+
+	if err := r.Get(ctx, req.NamespacedName, &upgradePlan); err != nil {
+		logger.Error(err, "unable to fetch UpgradePlan")
+
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	logger.Info("Upgrade to the SUSE platform requested",
+		"version", upgradePlan.Spec.ReleaseVersion)
 
 	return ctrl.Result{}, nil
 }
