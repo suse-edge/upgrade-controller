c4496cfd63c95f50fab80b59da00cbe07f4a618c
Ivo Petrov <ivo.petrov@suse.com>
2024-07-23 17:48:40 +0300

    Introduce OS control-plane plan object setup

--- internal/upgrade/os.go
+++ internal/upgrade/os.go
@@ -4,22 +4,25 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"path/filepath"
+	"strings"
 	"text/template"
 
+	upgradecattlev1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
 	"github.com/suse-edge/upgrade-controller/pkg/release"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	scriptName = "os-upgrade.sh"
+	secretName = "os-upgrade-secret"
 )
 
 //go:embed templates/os-upgrade.sh.tpl
 var osUpgradeScript string
 
 func OSUpgradeSecret(releaseOS *release.OperatingSystem) (*corev1.Secret, error) {
-	const (
-		scriptName = "os-upgrade.sh"
-		secretName = "os-upgrade-secret"
-	)
-
 	tmpl, err := template.New(scriptName).Parse(osUpgradeScript)
 	if err != nil {
 		return nil, fmt.Errorf("parsing contents: %w", err)
@@ -45,7 +48,7 @@ func OSUpgradeSecret(releaseOS *release.OperatingSystem) (*corev1.Secret, error)
 	}
 
 	secret := &corev1.Secret{
-		ObjectMeta: v1.ObjectMeta{
+		ObjectMeta: metav1.ObjectMeta{
 			Name:      secretName,
 			Namespace: planNamespace,
 		},
@@ -56,4 +59,71 @@ func OSUpgradeSecret(releaseOS *release.OperatingSystem) (*corev1.Secret, error)
 	}
 
 	return secret, nil
+}
+
+func OSControlPlanePlan(release *release.Release) *upgradecattlev1.Plan {
+	const (
+		planImage = "registry.suse.com/bci/bci-base:15.5"
+	)
+
+	controlPlanePlanName := osPlanName(controlPlaneKey, release.Components.OperatingSystem.ZypperID, release.Components.OperatingSystem.Version)
+
+	controlPlanePlan := baseUpgradePlan(controlPlanePlanName)
+	controlPlanePlan.Labels = map[string]string{
+		"os-upgrade": "control-plane",
+	}
+	controlPlanePlan.Spec.Concurrency = 1
+	controlPlanePlan.Spec.NodeSelector = &metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      ControlPlaneLabel,
+				Operator: "In",
+				Values: []string{
+					"true",
+				},
+			},
+		},
+	}
+	controlPlanePlan.Spec.Tolerations = []corev1.Toleration{
+		{
+			Key:      "CriticalAddonsOnly",
+			Operator: "Equal",
+			Value:    "true",
+			Effect:   "NoExecute",
+		},
+		{
+			Key:      ControlPlaneLabel,
+			Operator: "Equal",
+			Value:    "",
+			Effect:   "NoSchedule",
+		},
+		{
+			Key:      "node-role.kubernetes.io/etcd",
+			Operator: "Equal",
+			Value:    "",
+			Effect:   "NoExecute",
+		},
+	}
+
+	secretPathRelativeToHost := fmt.Sprintf("/run/system-upgrade/secrets/%s", secretName)
+	mountPath := filepath.Join("/host", secretPathRelativeToHost)
+	controlPlanePlan.Spec.Secrets = []upgradecattlev1.SecretSpec{
+		{
+			Name: secretName,
+			Path: mountPath,
+		},
+	}
+	controlPlanePlan.Spec.Cordon = true
+	controlPlanePlan.Spec.Version = release.ReleaseVersion
+
+	controlPlanePlan.Spec.Upgrade = &upgradecattlev1.ContainerSpec{
+		Image:   planImage,
+		Command: []string{"chroot", "/host"},
+		Args:    []string{"sh", filepath.Join(secretPathRelativeToHost, scriptName)},
+	}
+	return controlPlanePlan
+}
+
+func osPlanName(typeKey, osName, osVersion string) string {
+	return fmt.Sprintf("%s-%s-%s", typeKey, strings.ToLower(osName), strings.ReplaceAll(osVersion, ".", "-"))
 }
