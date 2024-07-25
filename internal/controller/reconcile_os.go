aead1cc90f67c9f42d2188e4010e3549efcfb73e
Ivo Petrov <ivo.petrov@suse.com>
2024-07-25 16:07:10 +0300

    Add basic control-plane Plan deployment logic

--- internal/controller/reconcile_os.go
+++ internal/controller/reconcile_os.go
@@ -4,23 +4,38 @@ import (
 	"context"
 	"fmt"
 
+	lifecyclev1alpha1 "github.com/suse-edge/upgrade-controller/api/v1alpha1"
 	"github.com/suse-edge/upgrade-controller/internal/upgrade"
 	"github.com/suse-edge/upgrade-controller/pkg/release"
+	"k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 //lint:ignore U1000 - Temporary ignore "unused" linter error. Will be removed when function is ready to be used.
-func (r *UpgradePlanReconciler) reconcileOS(ctx context.Context, releaseOS *release.OperatingSystem) (ctrl.Result, error) {
-	secret, err := upgrade.OSUpgradeSecret(releaseOS)
+func (r *UpgradePlanReconciler) reconcileOS(ctx context.Context, upgradePlan *lifecyclev1alpha1.UpgradePlan, release *release.Release) (ctrl.Result, error) {
+	secret, err := upgrade.OSUpgradeSecret(&release.Components.OperatingSystem)
 	if err != nil {
 		return ctrl.Result{}, fmt.Errorf("generating OS upgrade secret: %w", err)
 	}
 
-	if err = r.Create(ctx, secret); err != nil {
-		return ctrl.Result{}, fmt.Errorf("creating OS upgrade secret: %w", err)
+	if err = r.Get(ctx, client.ObjectKeyFromObject(secret), secret); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{}, r.createSecret(ctx, upgradePlan, secret)
 	}
 
-	// TODO: OS upgrade logic
+	controlPlanePlan := upgrade.OSControlPlanePlan(release.ReleaseVersion, &release.Components.OperatingSystem)
+	if err = r.Get(ctx, client.ObjectKeyFromObject(controlPlanePlan), controlPlanePlan); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+
+		setInProgressCondition(upgradePlan, lifecyclev1alpha1.OperatingSystemUpgradedCondition, "Control plane nodes are being upgraded")
+		return ctrl.Result{}, r.createPlan(ctx, upgradePlan, controlPlanePlan)
+	}
 
 	return ctrl.Result{Requeue: true}, nil
 }
