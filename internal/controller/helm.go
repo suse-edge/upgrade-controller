993d97c6bff2a43e100c1f403b822f1fbd449d0c
Atanas Dinov <atanas.dinov@suse.com>
2024-07-31 12:41:03 +0300

    Unify Helm chart upgrades
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- internal/controller/helm.go
+++ internal/controller/helm.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"slices"
 
 	lifecyclev1alpha1 "github.com/suse-edge/upgrade-controller/api/v1alpha1"
 	"github.com/suse-edge/upgrade-controller/internal/upgrade"
@@ -16,9 +17,14 @@ import (
 	helmstorage "helm.sh/helm/v3/pkg/storage"
 	helmdriver "helm.sh/helm/v3/pkg/storage/driver"
 
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 func newHelmClient() (*helmstorage.Storage, error) {
@@ -46,10 +52,11 @@ func retrieveHelmRelease(name string) (*helmrelease.Release, error) {
 
 	helmReleases, err := helmClient.History(name)
 	if err != nil {
-		if errors.Is(err, helmdriver.ErrReleaseNotFound) {
-			return nil, nil
-		}
 		return nil, err
+	}
+
+	if len(helmReleases) == 0 {
+		return nil, helmdriver.ErrReleaseNotFound
 	}
 
 	helmutil.Reverse(helmReleases, helmutil.SortByRevision)
@@ -115,4 +122,80 @@ func (r *UpgradePlanReconciler) createHelmChart(ctx context.Context, upgradePlan
 	}
 
 	return r.Create(ctx, chart)
+}
+
+func (r *UpgradePlanReconciler) upgradeHelmChart(ctx context.Context, upgradePlan *lifecyclev1alpha1.UpgradePlan, releaseChart *release.HelmChart) (upgrade.HelmChartState, error) {
+	helmRelease, err := retrieveHelmRelease(releaseChart.Name)
+	if err != nil {
+		if errors.Is(err, helmdriver.ErrReleaseNotFound) {
+			return upgrade.ChartStateNotInstalled, nil
+		}
+		return upgrade.ChartStateUnknown, fmt.Errorf("retrieving helm release: %w", err)
+	}
+
+	chart := &helmcattlev1.HelmChart{}
+
+	if err = r.Get(ctx, upgrade.ChartNamespacedName(helmRelease.Name), chart); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return upgrade.ChartStateUnknown, err
+		}
+
+		if helmRelease.Chart.Metadata.Version == releaseChart.Version {
+			return upgrade.ChartStateVersionAlreadyInstalled, nil
+		}
+
+		return upgrade.ChartStateInProgress, r.createHelmChart(ctx, upgradePlan, helmRelease, releaseChart)
+	}
+
+	if chart.Spec.Version != releaseChart.Version {
+		return upgrade.ChartStateInProgress, r.updateHelmChart(ctx, upgradePlan, chart, releaseChart)
+	}
+
+	releaseVersion := chart.Annotations[upgrade.ReleaseAnnotation]
+	if releaseVersion != upgradePlan.Spec.ReleaseVersion {
+		return upgrade.ChartStateVersionAlreadyInstalled, nil
+	}
+
+	job := &batchv1.Job{}
+	if err = r.Get(ctx, types.NamespacedName{Name: chart.Status.JobName, Namespace: upgrade.ChartNamespace}, job); err != nil {
+		return upgrade.ChartStateUnknown, err
+	}
+
+	idx := slices.IndexFunc(job.Status.Conditions, func(condition batchv1.JobCondition) bool {
+		return condition.Status == corev1.ConditionTrue &&
+			(condition.Type == batchv1.JobComplete || condition.Type == batchv1.JobFailed)
+	})
+
+	if idx == -1 {
+		// Upgrade job is still ongoing.
+		return upgrade.ChartStateInProgress, nil
+	}
+
+	condition := job.Status.Conditions[idx]
+	if condition.Type == batchv1.JobComplete {
+		return upgrade.ChartStateSucceeded, nil
+	}
+
+	logger := log.FromContext(ctx)
+	logger.Info("Helm chart upgrade job failed",
+		"helmChart", releaseChart.Name,
+		"job", fmt.Sprintf("%s/%s", job.Namespace, job.Name),
+		"jobStatus", condition.Message)
+
+	return upgrade.ChartStateFailed, nil
+}
+
+func evaluateHelmChartState(state upgrade.HelmChartState) (setCondition setCondition, requeue bool) {
+	switch state {
+	case upgrade.ChartStateNotInstalled, upgrade.ChartStateVersionAlreadyInstalled:
+		return setSkippedCondition, true
+	case upgrade.ChartStateInProgress:
+		return setInProgressCondition, false
+	case upgrade.ChartStateSucceeded:
+		return setSuccessfulCondition, true
+	case upgrade.ChartStateFailed:
+		return setFailedCondition, true
+	default:
+		return nil, false
+	}
 }
