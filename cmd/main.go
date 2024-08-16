b0df33c9f0f1da5729f9fe526acf25096e47e385
Atanas Dinov <atanas.dinov@suse.com>
2024-08-16 10:33:11 +0300

    Linter fix
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- cmd/main.go
+++ cmd/main.go
@@ -75,7 +75,8 @@ func main() {
 		"If set the metrics endpoint is served securely")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	flag.StringVar(&watchNamespace, "namespace", os.Getenv("WATCH_NAMESPACE"), "Namespace that the controller watches to reconcile resources.")
+	flag.StringVar(&watchNamespace, "namespace", os.Getenv("WATCH_NAMESPACE"),
+		"Namespace that the controller watches to reconcile resources.")
 	opts := zap.Options{
 		Development: true,
 	}
