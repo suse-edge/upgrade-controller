705093f36b8f53957bb0a837ffa6ccc288168dcd
Atanas Dinov <atanas.dinov@suse.com>
2024-10-10 12:05:03 +0300

    Improve node matching (#98)
    * Improve node matching during Kubernetes upgrades
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    * Improve node matching during OS upgrades
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>
    
    ---------
    
    Signed-off-by: Atanas Dinov <atanas.dinov@suse.com>

--- internal/controller/reconcile_os_test.go
+++ internal/controller/reconcile_os_test.go
@@ -8,7 +8,6 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 )
 
 func TestFindUnsupportedNodes(t *testing.T) {
@@ -49,133 +48,101 @@ func TestFindUnsupportedNodes(t *testing.T) {
 func TestIsOSUpgraded(t *testing.T) {
 	const osPrettyName = "SUSE Linux Micro 6.0"
 
-	nodeLabels := map[string]string{
-		"node-x": "z",
-	}
-
 	tests := []struct {
 		name            string
-		nodes           *corev1.NodeList
-		selector        labels.Selector
+		nodes           []corev1.Node
 		expectedUpgrade bool
 	}{
 		{
-			name: "All matching nodes upgraded",
-			nodes: &corev1.NodeList{
-				Items: []corev1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Enterprise Micro 5.5"}},
-					},
+			name: "All nodes upgraded",
+			nodes: []corev1.Node{
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
 				},
 			},
-			selector:        labels.SelectorFromSet(nodeLabels),
 			expectedUpgrade: true,
 		},
 		{
-			name: "Unschedulable matching node",
-			nodes: &corev1.NodeList{
-				Items: []corev1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: true},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Enterprise Micro 5.5"}},
-					},
+			name: "Unschedulable node",
+			nodes: []corev1.Node{
+				{
+					Spec: corev1.NodeSpec{Unschedulable: true},
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
 				},
 			},
-			selector:        labels.SelectorFromSet(nodeLabels),
 			expectedUpgrade: false,
 		},
 		{
-			name: "Not ready matching node",
-			nodes: &corev1.NodeList{
-				Items: []corev1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionFalse}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Enterprise Micro 5.5"}},
-					},
+			name: "Not ready node",
+			nodes: []corev1.Node{
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionFalse}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
 				},
 			},
-			selector:        labels.SelectorFromSet(nodeLabels),
 			expectedUpgrade: false,
 		},
 		{
-			name: "Matching node on older OS",
-			nodes: &corev1.NodeList{
-				Items: []corev1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro Micro 5.5"}},
-					},
-					{
-						ObjectMeta: metav1.ObjectMeta{Labels: nodeLabels},
-						Spec:       corev1.NodeSpec{Unschedulable: false},
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
-					},
-					{
-						Status: corev1.NodeStatus{
-							Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
-							NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Enterprise Micro 5.5"}},
-					},
+			name: "Node on older OS",
+			nodes: []corev1.Node{
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro Micro 5.5"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Micro 6.0"}},
+				},
+				{
+					Status: corev1.NodeStatus{
+						Conditions: []corev1.NodeCondition{{Type: corev1.NodeReady, Status: corev1.ConditionTrue}},
+						NodeInfo:   corev1.NodeSystemInfo{OSImage: "SUSE Linux Enterprise Micro 5.5"}},
 				},
 			},
-			selector:        labels.SelectorFromSet(nodeLabels),
 			expectedUpgrade: false,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.expectedUpgrade, isOSUpgraded(test.nodes, test.selector, osPrettyName))
+			assert.Equal(t, test.expectedUpgrade, isOSUpgraded(test.nodes, osPrettyName))
 		})
 	}
 }
